2626f00c2b91427afe3926867f51019ec2a90664
Alain Sanchez <salain919@gmail.com>
2020-02-06 15:00:09 -0700

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:Hw1_QuizApp/Hw1_QuizApp.dart' as Hw1_QuizApp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Hw1_QuizApp.calculate()}!');
+}
---
+++ test/Hw1_QuizApp_test.dart
@@ -0,0 +1,8 @@
+import 'package:Hw1_QuizApp/Hw1_QuizApp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
