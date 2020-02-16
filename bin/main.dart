262243dc7d575c46597ee4a58de57a2cda8c73cb
Alain Sanchez <salain919@gmail.com>
2020-02-16 16:57:37 -0700

    WebClient.dart now obtains a list of questions for available quizzes.

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,8 @@
 import 'package:Hw1_QuizApp/QuestionsDisplayer.dart' as io;
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:Hw1_QuizApp/WebClient.dart';
 
 void main(List<String> arguments) async{
-  var url = 'http://www.cs.utep.edu/cheon/cs4381/homework/quiz/?quiz=quiz02';
-  var response = await http.get(url);
-//  print('Response status: ${response.statusCode}');
-//  print('Response body: ${response.body}');
-  var decode = json.decode(response.body);
-  io.beginQuiz(decode);
+  var wc = WebClient();
+  await wc.getQuestions();
+//  io.beginQuiz(await wc.getQuestions());
 }
--- lib/WebClient.dart
+++ lib/WebClient.dart
@@ -1,1 +1,22 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+class WebClient {
+  Future getQuestions () async{
+    var Questions = [];
+    var url = 'http://www.cs.utep.edu/cheon/cs4381/homework/quiz/?quiz=quiz01';
+    var response = await http.get(url);
+    var decode = json.decode(response.body);
+
+    var i = 1;
+    while (decode['response']) {
+      Questions.add(decode['quiz']['question']);
+      url = 'http://www.cs.utep.edu/cheon/cs4381/homework/quiz/?quiz=quiz0' +i.toString();
+      response = await http.get(url);
+      decode = json.decode(response.body);
+      i++;
+    }
+    // Questions.forEach((e) => print(e));
+    return Questions;
+  }
+}
