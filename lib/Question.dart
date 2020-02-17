9295eb24a670f40a413b9ba8c280067e35e420c9
sebnunez6 <snunez6@miners.utep.edu>
2020-02-16 18:59:48 -0700

    setters and getters implemented for Question.dart

--- lib/Question.dart
+++ lib/Question.dart
@@ -1,8 +1,40 @@
 class Question{
   var _question;
   var _possibleAnswers;
-  var questionAnswer;
+  var _questionAnswer;
   var _isCorrect = false;
   Question(this._question, this._possibleAnswers);
 
+  void set question(var question){
+    this._question = question;
+  }
+
+  void set possibleAnswers(var possibleAnswers){
+    this._possibleAnswers = possibleAnswers;
+  }
+
+  void set questionAnswer(var questionAnswers){
+    this._questionAnswer = questionAnswers;
+    isCorrect();
+  }
+
+  void isCorrect(){
+    this._isCorrect = _possibleAnswers == _questionAnswer;
+  }
+
+  String getquestion(){
+    return _question;
+  }
+
+  String getpossibleAnswers(){
+    return _possibleAnswers;
+  }
+
+  String getquestionAnswer(){
+    return _questionAnswer;
+  }
+
+  bool getisCorrect(){
+    return _isCorrect;
+  }
 }
